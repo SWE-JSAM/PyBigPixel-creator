c54f893734d170e94358a664180abff7367801f1
SWE-JSAM <samuelssonjorgen@gmail.com>
2014-08-10 17:54:33 +0200

    Languish settings added dev version 0.2.2

--- pybigpixel/prepare.py
+++ pybigpixel/prepare.py
@@ -1,11 +1,70 @@
-'''
-Created on 2 aug 2014
+# PyBigPixel Creator software converts your digital images to pixel patterns.
+#
+# PyBigPixel Creator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBigPixel Creator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyBigPixel Creator. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-@author: jorgen
-'''
-LANG = {'sv_SE': "pybigpixel_sv_SE.qm", 'en_US': 'Default', 'en_UK': 'Default'}
-def configure():
-    pass
+import configparser
+import os
+
+LANGUISH = {'sv_SE': {'Lang': 'Swedish', 'file': 'pybigpixel_sv_SE.qm'},
+            'default': {'Lang': 'English', 'file': 'default'}}
+
+
+class Config(object):
+    """Read, write / create config file."""
+    def __init__(self):
+        config_dir = os.path.expanduser('~/.config')
+        self.config_dir = os.path.join(config_dir, 'pybigpixel')
+        if not os.path.isdir(self.config_dir):
+            os.mkdir(self.config_dir)
+        self.config_file = os.path.join(self.config_dir, 'pybigpixel.conf')
+
+        self.config = configparser.ConfigParser()
+
+    def read_config(self):
+        self.config.read(self.config_file)
+        com = self.config['Common']
+        shape = str(com['shape'])
+        background = str(com['background'])
+        width_pixels = int(com['width_pixels'])
+        height_pixels = int(com['height_pixels'])
+        languish = str(com['languish'])
+        version = str(com['version'])
+        return (shape, background, width_pixels, height_pixels, languish,
+                version)
+
+    def write_config(self, shape, background, width_pixels, height_pixels,
+                     languish, version):
+        self.config['Common'] = {}
+        com = self.config['Common']
+        com['shape'] = str(shape)
+        com['background'] = str(background)
+        com['width_pixels'] = str(width_pixels)
+        com['height_pixels'] = str(height_pixels)
+        com['languish'] = str(languish)
+        com['version'] = str(version)
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile)
+
+    def get_languish(self):
+        self.config.read(self.config_file)
+        com = self.config['Common']
+        languish = str(com['languish'])
+        return languish
+
 
 if __name__ == '__main__':
-    pass
+    conf = Config()
+    conf.write_config('squares', 'gray', '30', '30', 'English', '0.2.2')
+    print(conf.read_config())
+    print(conf.get_languish())
